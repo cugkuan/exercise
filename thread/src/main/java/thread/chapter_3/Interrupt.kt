7a79b2d132fa84788e4b440b3e592995d90d4061
kuan <cugkuan@163.com>
2022-10-14 17:39:49 +0800

    添加线程被打断的例子

---
+++ thread/src/main/java/thread/chapter_3/Interrupt.kt
@@ -0,0 +1,24 @@
+package thread.chapter_3
+
+
+
+
+fun main(){
+    val lock = Object()
+    val t0 = Thread(Runnable {
+        synchronized(lock){
+            try {
+                lock.wait()
+            }catch (e:InterruptedException){
+                println("线程被打断了")
+            }
+        }
+    })
+    val t1 = Thread(Runnable {
+        Thread.sleep(2 * 1000)
+        t0.interrupt()
+    })
+    t1.start()
+    t0.start()
+
+}
