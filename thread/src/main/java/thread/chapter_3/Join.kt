f4274888f4700113aea35fa7659d338ac0a3a460
kuan <cugkuan@163.com>
2022-10-14 18:01:00 +0800

    join 例子加强

--- thread/src/main/java/thread/chapter_3/Join.kt
+++ thread/src/main/java/thread/chapter_3/Join.kt
@@ -5,16 +5,29 @@ import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
     val t1 = Thread {
-        println("dddddd")
+        println("查询1")
         TimeUnit.SECONDS.sleep(5)
-        println("线程t1结束")
+        println("查询1结束")
     }
     val t2 = Thread {
-        println("aaaaaa")
-        t1.join()
-        println("线程t2结束")
+        println("查询2")
+        TimeUnit.SECONDS.sleep(1)
+        println("查询2结束")
     }
-    t1.start()
-    t2.start()
+    val t3 = Thread{
+        println("查询3")
+        TimeUnit.SECONDS.sleep(2)
+        println("查询3结束")
+    }
+
+    Thread(){
+        t1.start()
+        t2.start()
+        t3.start()
+        t1.join()
+        t2.join()
+        t3.join()
+        println("整理完查询信息")
+    }.start()
 }
 
