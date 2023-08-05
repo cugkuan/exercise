38db3881b8dc3d425e46f92b73bf9ebbe207bc3d
kuan <cugkuan@163.com>
2023-08-05 15:42:43 +0800

    Modify(算法)：牛客网，今日头条的考题7，机器人跳跃问题

---
+++ lectcode/src/main/java/exercise/Byte7.java
@@ -0,0 +1,34 @@
+package exercise;
+
+
+import java.util.Scanner;
+
+/**
+
+ *设置 H（i）为第 i个建筑的能量值。
+ * E（i）为 第 i 个建筑 的剩余能量；
+ * 于是，E（i+1） = 2E(i) - H(i+1)
+ * 于是 有  E(i) = ( H(i+1) + E(i+1))/2
+ * 我们从从后面往前面推。理想的情况下，最后跳完后 能量为 0 于是 往前面倒推
+ */
+public class Byte7 {
+
+    public static int e(int[] h){
+        int e = 0;
+        for (int i = h.length-1;i >= 0;i--){
+            e =  (int)Math.ceil((h[i] + e)/2f);
+        }
+        return e;
+    }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] h = new int[n];
+        int i = 0;
+        while (i <n){
+            h[i] = scanner.nextInt();
+            i++;
+        }
+        System.out.println(e(h));
+    }
+}
